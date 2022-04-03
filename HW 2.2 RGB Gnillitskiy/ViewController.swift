7786efc6f23206411d555a95d4396825fc874e6f
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-04-03 22:28:08 +0300

    Code ready

--- HW 2.2 RGB Gnillitskiy/ViewController.swift
+++ HW 2.2 RGB Gnillitskiy/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+
+    @IBOutlet var viewRGB: UIView!
+  
+    @IBOutlet var redInfo: UILabel!
+    @IBOutlet var greenInfo: UILabel!
+    @IBOutlet var blueInfo: UILabel!
+    
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewRGB.backgroundColor = .black
     }
-
-
+    
+    @IBAction func mixColor(_ sender: UISlider) {
+        viewRGB.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderBlue.value), blue: CGFloat(sliderGreen.value), alpha: 1)
+       
+        redInfo.text = String(round(sliderRed.value * 100) / 100)
+        greenInfo.text = String(round(sliderGreen.value * 100) / 100)
+        blueInfo.text = String(round(sliderBlue.value * 100) / 100)
+    }
 }
+
+
+
+
 
